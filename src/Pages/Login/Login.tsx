948c3e070c9eab727c1c2dd438c6acf1d42707d9
kirioka <hei.amour.2@gmail.com>
2022-09-06 22:24:35 +0300

    draft: login page

---
+++ src/Pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import {Autocomplete, Button, Dialog, Pagination, Pane, TextInput} from "evergreen-ui";
+
+const Login: FC = () => {
+    const [isShown, setIsShown] = React.useState(false);
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Login;
